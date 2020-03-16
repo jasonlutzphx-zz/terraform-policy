b8840c1d2913acfba14d739b7f593a546d55c435
jasonlutzphx <jasonlutzphx@gmail.com>
2020-03-16 12:14:45 -0700

    update Jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,5 +1,13 @@
 node {
-    stage('hello') {
-        echo 'Hello, World'
+    checkout scm
+    docker.image('hashicorp/terraform:0.12.20').inside('--rm --entrypoint=""') {
+        withEnv(['TF_IN_AUTOMATION=true', 'TF_INPUT=false']) {
+            stage('initialize') {
+                sh 'terraform init'
+            }
+            stage('plan') {
+                sh 'terraform plan'
+            }
+        }
     }
 }
---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-policy-jasonlutzphx"
+    key            = "default/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-policy-jasonlutzphx"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  version = "~> 2"
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "toy-resource-jasonlutzphx"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
