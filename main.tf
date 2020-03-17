b29116c6a2c45dbde43dadbeebf5fec89e2b66af
jasonlutzphx <jasonlutzphx@gmail.com>
2020-03-17 06:05:15 -0700

    destroy bucket

--- main.tf
+++ main.tf
@@ -13,13 +13,4 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "toy-resource-jasonlutzphx3"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
 
